f80cc42a2360ad36bba02bbbeb3d3bddd26a5a5e
Jxiang-jc <zjx1020645@163.com>
2019-01-09 21:16:51 +0800

    完善面向对象写法,并增加拖拽功能

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(function () {
 
     // 创建对象
     class TableDemo {
-        constructor(obj) {
+        constructor(obj = null) {
             this.$table = $('#table')
             this.$btnSearch = $('#search_btn')                  // 搜索按钮
             this.$addForm = $('#addForm')                       // 添加表单
@@ -10,16 +10,26 @@ $(function () {
             this.$add_backBtn = $('#add_backBtn')               // 新增页面的返回按钮
             this.$tableBody = $('.tableBody')                   // 主页面
             this.$addBody = $('.addBody')                       // 新增页面
+            this.popup_de = document.querySelector('.popup_de') // 弹窗盒子
+            this.overlay = document.querySelector('.overlay')   // 半透明遮罩盖
+
+            // 初始化
+            this.TableInit(obj)
+
+            // 表单验证
+            this.bootstrapValidator()
 
             // 增加按钮事件
             this.$btn_add.click(() => {
                 
-                console.log($('.tableBody'))
                 this.$tableBody.addClass('animated slideOutLeft')
+
                 setTimeout(() => {
                     this.$tableBody.removeClass('animated slideOutLeft').css('display', 'none')
                 }, 500)
+
                 this.$addBody.css('display', 'block')
+
                 this.$addBody.addClass('animated slideInRight')
             })
 
@@ -37,31 +47,53 @@ $(function () {
                 this.$addForm.data('bootstrapValidator').resetForm(true)
             })
 
-            // 初始化
-            this.TableInit(obj)
+            // 删除按钮与修改按钮的出现与消失
+            this.$table.change(() => { // change() 只针对改变表单内容会触发
+                this.delAndEditBtn()
+            })
 
-            // 表单验证
-            this.bootstrapValidator()
+            // 关闭弹窗
+            this.popup_de.onclick = e => {
+
+                let eventClass = $(e.target).attr('class')
+
+                // 弹出框取消按钮 + 弹出框关闭按钮
+                if (eventClass === 'popup_btn btn_cancel' || eventClass === 'popup_close') {
+                    this.close()
+                }
+            }
+
+            // ESC关闭
+            document.onkeyup = e =>{
+                if(e.keyCode === 27){
+                    this.close();
+                }
+            }
+
+            // 可拖拽
+            this.draggable()
         }
 
+        // 初始化表格
         TableInit (obj) {
+            // var url = obj.url 可以这么传
             this.$table.bootstrapTable({
                 url: './api/bootstrapTable.php',
-                toolbar: '#toolbar',        // 指定工作栏
-                toolbarAlign: 'right',      // 工具栏对齐方式
-                buttonsAlign: 'right',      // 按钮对齐方式
-                showRefresh: true,          // 刷新按钮
-                showColumns: true,          // 是否显示内容列下拉框。
-                showToggle: true,           // 是否显示切换视图（table/card）按钮。
-                striped: true,              // 是否显示行间隔色
-                clickToSelect: false,        // 是否启用点击选中行
-                cache: false,               // 是否使用缓存
-                uniqueId: "ID",             // 每一行的唯一标识，一般为主键列
-                pageNumber: 1,              // 初始化加载第一页，默认第一页
-                pageSize: "5",              // 每页显示
-                pageList: [5, 10, 20, 30],  // 分页步进值
-                pagination: true,           // 是否分页
-                sortable: false,            // 是否启用排序
+                toolbar: '#toolbar',             // 指定工作栏
+                toolbarAlign: 'right',           // 工具栏对齐方式
+                buttonsAlign: 'right',           // 按钮对齐方式
+                showRefresh: true,               // 刷新按钮
+                showColumns: true,               // 是否显示内容列下拉框。
+                showToggle: true,                // 是否显示切换视图（table/card）按钮。
+                striped: true,                   // 是否显示行间隔色
+                clickToSelect: false,            // 是否启用点击选中行
+                cache: false,                    // 是否使用缓存
+                uniqueId: "ID",                  // 每一行的唯一标识，一般为主键列
+                pageNumber: 1,                   // 初始化加载第一页，默认第一页
+                pageSize: "5",                   // 每页显示
+                pageList: [5, 10, 20, 30],       // 分页步进值
+                pagination: true,                // 是否分页
+                sortable: false,                 // 是否启用排序
                 queryParams: this.queryParams,   // 请求服务器时所传的参数
                 columns: [
                     {
@@ -116,10 +148,8 @@ $(function () {
                 // },
             })
             // 等待数据渲染以后才能做怎增增删改查
-            this.del()
+            this.judgeActions()
 
-            // 删除按钮与修改按钮的出现与消失
-            this.change()
         };
 
         // 操作栏的格式化
@@ -138,7 +168,7 @@ $(function () {
 
         // 请求服务数据时所传参数
         queryParams (params) {
-            console.log(params)
+            // console.log(params)
             var temp = {
                 // pageSize: params.limit,
                 // pageIndex: params.pageNumber,
@@ -237,101 +267,176 @@ $(function () {
                     }
                 }
             })
-        };
-
-        // 删除按钮事件
-        del () {
-            $('.bootstrap-table').on('click', '#btn_delete, span', (e) => {
-                var e = e || window.event
-                e = e.srcElement || e.target
-                // 单个删除按钮
-                if ($(e).hasClass('delBtn2')) {
-                    // console.log($(e).closest('tr').find('input').prop('checked', !this.checked))
-
-                    // 修改状态
-                    // $(e).closest('tr').find('input').prop('checked', false)
-                    $(e).closest('tr').removeClass('selected')
-                    this.checkboxLen()
-                    $(e).closest('tr').remove()
-                }
-        
-                // 可多个删除的按钮
-                if ($(e).hasClass('delBtn') || $(e).hasClass('delBtn1')) {
-                    // 获取勾选的数量
-                    // console.log(dataArr)
-                    var dataArr = this.$table.bootstrapTable('getSelections')
-                    
-                    $('.popup_de .show_msg').text('确定要删除该用户吗?')
-                    $('.popup_de').addClass('bbox');
-        
-                    // one只执行一次
-                    $('.popup_de .btn_submit').one('click', function () {
-        
-                        // 遍历勾选中的行数，找到它的id，并传到后台删掉，由于我这里不做后台数据库的操作，也就没有写了
-                        var ID = []
-                        
-                        for (var i = 0; i < dataArr.length; i++) {
-                            ID[i] = dataArr[i].ID
-                        }
-                        // $.get('url', {Id: ID}, (data)=>{})
-                        $('.popup_de .show_msg').text('删除成功！')
-                        $('.popup_de .btn_cancel').hide() // 隐藏取消按钮
-                        
-                        $('.popup_de .btn_submit').one('click', () => {
-                            $('.popup_de .btn_cancel').show()
-                            $('.popup_de').removeClass('bbox')
-                            //获取勾选的input.并删除
-                            var $checked = $('.bs-checkbox>input').filter   (':checked')
-
-                            $(e).closest('tr').removeClass('selected')
-                            this.checkboxLen()
-                            $checked.closest('tr').remove();
-                        })
-        
-                    }.bind(this))
-                    // 弹出框取消按钮事件
-                    $('.popup_de .btn_cancel').click(function () {
-                        $('.popup_de').removeClass('bbox');
-                    })
-                    // 弹出框关闭按钮事件
-                    $('.popup_de .popup_close').click(function () {
-                        $('.popup_de').removeClass('bbox');
-                    })
-                }
-            })
+            return this
         };
 
         // 删除按钮与修改按钮的出现与消失
-        change () {
-            this.$table.children('tbody').change(() => {
-                this.checkboxLen()
-            })
-        };
+        delAndEditBtn () {
+            // 获取勾选中的数量
+            let dataArrLen = $('#table .selected').length
 
-        checkboxLen () {
-            var dataArrLen = $('#table .selected').length
+            // toolbar里面的编辑与删除按钮
+            let $btn_edit = $('#btn_edit')                      
+            let $btn_delete = $('#btn_delete')
 
             if (dataArrLen === 1) {
-                $('#btn_edit').css('display', 'block').removeClass('fadeOutRight').addClass('animated fadeInRight');
+                $btn_edit.show().removeClass('fadeOutRight').addClass('animated fadeInRight');
             } else {
-                $('#btn_edit').addClass('fadeOutRight')
+                $btn_edit.addClass('fadeOutRight')
                 setTimeout( () => {
-                    $('#btn_edit').css('display', 'none')
+                    $btn_edit.hide()
                 }, 400)
             }
     
             if (dataArrLen >= 1) {
-                $('#btn_delete').css('display', 'block').removeClass('fadeOutRight').addClass('animated fadeInRight')
+                $btn_delete.css('display', 'block').removeClass('fadeOutRight').addClass('animated fadeInRight')
             } else {
-                $('#btn_delete').addClass('fadeOutRight')
+                $btn_delete.addClass('fadeOutRight')
                 setTimeout( () => {
-                    $('#btn_delete  ').css('display', 'none')
+                    $btn_delete.css('display', 'none')
                 }, 400)
             }
-        }
+        };
+
+        // 判断删除按钮
+        judgeActions () {
+            $('.bootstrap-table').on('click', '#btn_delete, span', (e) => {
+                let eve = e || window.event
+                eve = e.srcElement || e.target
+                
+                // 单个删除按钮
+                if ($(eve).hasClass('delBtn2')) {
+
+                    this.open($(eve))
+
+                } else if ($(eve).hasClass('delBtn') || $(eve).hasClass('delBtn1')) { // 可多个删除的按钮
+                    
+                    this.open()
+
+                }
+            })
+        };
+
+        // 确认删除
+        delete ($event = null) {
+            
+            // 获取勾选的数量
+            // var dataArr = this.$table.bootstrapTable('getSelections')
+            // 遍历勾选中的行数，找到它的id，并传到后台删掉，由于我这里不做后台数据库的操作，也就没有写了
+            // let ID = []
+            // for (var i = 0; i < dataArr.length; i++) {
+            //     ID[i] = dataArr[i].ID
+            // }
+            // $.get('url', {Id: ID}, (data)=>{})
+
+            this.$show_msg.text('删除成功！')
+
+            $('.popup_de .btn_cancel').hide() // 隐藏取消按钮
+            
+            $('.popup_de .btn_submit').one('click', () => {
+                $('.popup_de .btn_cancel').show()
+                
+                this.close()
+
+                // 判断是否传入event.target, 也是就判断是点了哪个删除按钮
+                let $checkedTarget = $event || $('.bs-checkbox>input').filter(':checked') 
+                
+                if ($checkedTarget) {
+                    // 修改状态为了触发bootstrapTable的toolbar事件
+                    $checkedTarget.closest('tr').removeClass('selected')
+
+                    // 删除按钮与修改按钮的出现与消失
+                    this.delAndEditBtn()
+
+                    // 删除选中行
+                    $checkedTarget.closest('tr').remove()
+                } else {
+
+                }
+            })
+        }  
+
+        // 打开弹窗
+        open (event = null) {
+            let $show_msg = $('.popup_de .show_msg')
+
+            // 为了全局使用
+            this.$show_msg = $show_msg
+
+            // 给弹窗盒子添加类名,使其出现弹窗效果,这是居中的关键!!!!
+            this.popup_de.classList.add('bbox')
+
+            this.overlay.style.visibility = 'visible'
+
+            // 重置内容
+            $show_msg.text('确定要删除该用户吗?')
+
+            // one只执行一次
+            $('.popup_de .btn_submit').one('click', function () {
+
+                // 删除
+                this.delete(event)
+
+            }.bind(this))
+
+            return this
+        };
+
+        // 关闭弹窗
+        close () {
+            $('.popup_de').removeClass('bbox')
+
+            // 隐藏遮罩盖
+            this.overlay.style.visibility  = 'hidden'
+            
+            // 重置弹窗位置为居中
+            this.pos({left: 0, top: 0})
+
+            return this
+        };
+
+        // 拖拽弹窗
+        draggable () {
+            let title = this.popup_de.querySelector('h4')
+
+            title.onmousedown = mouseEve => {
+                // 获取鼠标到弹窗左上角距离
+                let popWidth = mouseEve.clientX - this.popup_de.offsetLeft
+                let popHeight = mouseEve.clientY - this.popup_de.offsetTop
+
+                document.onmousemove = evt => {
+                    // 拖动位置
+                    this.pos({
+                        left: evt.clientX - popWidth,
+                        top: evt.clientY - popHeight
+                    })
+    
+                    evt.preventDefault()
+                }
+                mouseEve.preventDefault()
+            }
+
+            title.onmouseup = () => {
+                document.onmousemove = null
+            }
+
+            return this
+        };
+        
+        // 弹窗位置
+        pos (pos) {
+            // 解构
+            let {left, top} = pos
+
+            $(this.popup_de).css('left', left) // jq
+            this.popup_de.style.top = top + 'px' // 原生js
+
+            return this
+
+        };
     }
 
-    var tableLise = new TableDemo()
+    var tableLise = new TableDemo(obj = null)
 
     // 查询按钮事件
     // $('#search_btn').click(function () {
@@ -371,4 +476,3 @@ cardView: false,                    //是否显示详细视图
 detailView: false,                   //是否显示父子表 
 
 */
-
